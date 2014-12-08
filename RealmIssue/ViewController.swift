72e0874834567caf51e0f2d8d4b2026b2eae0833
John Clayton <john@codemonkeylabs.com>
2014-12-07 22:35:12 -0700

    Lazy property works

--- RealmIssue/ViewController.swift
+++ RealmIssue/ViewController.swift
@@ -14,11 +14,17 @@ class ViewController: UIViewController {
     var variableRealm = RLMRealm.defaultRealm()
     let constantRealm = RLMRealm.defaultRealm()
 
+    lazy var lazyRealm: RLMRealm = {
+        return RLMRealm.defaultRealm()
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         println("[ViewController.viewDidLoad] Default realm path set to \(RLMRealm.defaultRealmPath())")
         println("[ViewController.viewDidLoad] Default realm instance path is \(RLMRealm.defaultRealm().path)")
+
+        println("[ViewController.viewDidLoad] Default realm lazy variable path is \(lazyRealm.path)")
 
         // This has the wrong path!
         println("[ViewController.viewDidLoad] Default realm variable path is \(variableRealm.path)")
